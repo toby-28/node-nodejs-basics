35892069e4ad59f6e14653c5adf7f9cfbe8a6ef9
hushnud <64332592+toby-28@users.noreply.github.com>
2025-10-20 20:26:26 +0500

    Create esm.mjs for server and JSON imports
    Implement ESM module with server setup and JSON handling

---
+++ src/modules/esm.mjs
@@ -0,0 +1,37 @@
+import path from 'node:path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+import './files/c.cjs' assert { type: 'javascript' };
+
+import aJson from './files/a.json' assert { type: 'json' };
+import bJson from './files/b.json' assert { type: 'json' };
+
+const random = Math.random();
+const unknownObject = random > 0.5 ? aJson : bJson;
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+// These are not available in ESM by default
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
+
+export { unknownObject, myServer };
