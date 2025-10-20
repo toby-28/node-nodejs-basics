8151277e9935c37aaeccc17c2a68880db98ed4ea
hushnud <64332592+toby-28@users.noreply.github.com>
2025-10-20 18:56:44 +0500

    Implement file listing in src/fs/list.js
    Added functionality to list files in the 'files' directory and handle errors.

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { readdir, access } from 'fs/promises';
+import { constants } from 'fs';
+import path from 'path';
+
 const list = async () => {
-  // Write your code here
+  const dirPath = path.join('files');
+
+  try {
+    await access(dirPath, constants.F_OK);
+    const files = await readdir(dirPath);
+    console.log(files);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
