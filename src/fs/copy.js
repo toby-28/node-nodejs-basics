1bb6c3410dfcd79b92b0dab38391d01a8842a4ec
hushnud <64332592+toby-28@users.noreply.github.com>
2025-10-20 18:52:10 +0500

    Add copy function to copy files recursively
    Implement file copy functionality with error handling.

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { access, cp } from 'fs/promises';
+import { constants } from 'fs';
+import path from 'path';
+
 const copy = async () => {
-  // Write your code here
+  const source = path.join('files');
+  const destination = path.join('files_copy');
+
+  try {
+    await access(source, constants.F_OK);
+    await access(destination, constants.F_OK);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await cp(source, destination, { recursive: true });
+    } else {
+      throw err;
+    }
+  }
 };
 
 await copy();
