fd3968e2eeeb8fa85d501b2249d8d2332002521c
hushnud <64332592+toby-28@users.noreply.github.com>
2025-10-20 21:40:35 +0500

    Refactor spawnChildProcess to use child_process
    Updated spawnChildProcess to utilize the child_process module for improved process management and added argument handling.

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
-const spawnChildProcess = async (args) => {
-  // Write your code here
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+export const spawnChildProcess = async (args) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const scriptPath = path.join(__dirname, 'files', 'script.js');
+
+  const child = spawn('node', [scriptPath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit']
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['hello', 'world']);
