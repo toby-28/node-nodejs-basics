18df431ac393fb9963a5ecf76f6055228b2cd5aa
hushnud <64332592+toby-28@users.noreply.github.com>
2025-10-20 18:55:42 +0500

    Implement file deletion using fs/promises
    Refactor remove function to use fs/promises for file deletion.

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { unlink, access } from 'fs/promises';
+import { constants } from 'fs';
+import path from 'path';
+
 const remove = async () => {
-  // Write your code here
+  const filePath = path.join('files', 'fileToRemove.txt');
+
+  try {
+    await access(filePath, constants.F_OK);
+    await unlink(filePath);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
