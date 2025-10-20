98dc131d3e6a3325eb5d751a013b674e632a66dc
hushnud <64332592+toby-28@users.noreply.github.com>
2025-10-20 21:22:57 +0500

    Add file reading functionality with error handling
    Implement file reading using streams and handle errors.

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  const stream = createReadStream(filePath, 'utf-8');
+  stream.pipe(process.stdout);
+
+  stream.on('error', () => {
+    console.error('FS operation failed');
+  });
 };
 
 await read();
