183d96b91ba81a3c33d37166380f60a6d6105b26
hushnud <64332592+toby-28@users.noreply.github.com>
2025-10-20 18:54:17 +0500

    Implement file renaming with error handling
    Refactor rename function to rename a file and handle errors.

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
-const rename = async () => {
-  // Write your code here
+import { rename, access } from 'fs/promises';
+import { constants } from 'fs';
+import path from 'path';
+
+const renameFile = async () => {
+  const oldPath = path.join('files', 'wrongFilename.txt');
+  const newPath = path.join('files', 'properFilename.md');
+
+  try {
+    await access(oldPath, constants.F_OK);
+    await access(newPath, constants.F_OK);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await rename(oldPath, newPath);
+    } else {
+      throw err;
+    }
+  }
 };
 
-await rename();
+await renameFile();
