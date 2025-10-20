63a479240d3b276cd2d6e22471d43f73d574452e
hushnud <64332592+toby-28@users.noreply.github.com>
2025-10-20 21:18:07 +0500

    Implement SHA-256 hash calculation with streams
    Refactor hash calculation to use streams and handle file reading errors.

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
-const calculateHash = async () => {
-  // Write your code here
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const calcHash = async () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('error', () => {
+    console.error('FS operation failed');
+  });
+
+  stream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on('end', () => {
+    console.log(hash.digest('hex'));
+  });
 };
 
-await calculateHash();
+await calcHash();
