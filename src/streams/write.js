fe96dfad7eb3aa53984e2abec152c544e5f97db1
hushnud <64332592+toby-28@users.noreply.github.com>
2025-10-20 21:23:24 +0500

    Implement file writing using streams
    Refactor write function to handle file writing from stdin.

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+  const stream = createWriteStream(filePath);
+  process.stdin.pipe(stream);
+
+  stream.on('error', () => {
+    console.error('FS operation failed');
+  });
 };
 
 await write();
