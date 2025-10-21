f35b4dfdafe1ec465b59154b08fbebbc964cf090
toby-28 <tobymarshal2802@gmail.com>
2025-10-21 13:10:38 +0500

    Refactor string quotes in calcHash.js for consistency

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,26 +1,26 @@
-import { createHash } from 'crypto';
-import { createReadStream } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const calcHash = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
 
-  const hash = createHash('sha256');
+  const hash = createHash("sha256");
   const stream = createReadStream(filePath);
 
-  stream.on('error', () => {
-    console.error('FS operation failed');
+  stream.on("error", () => {
+    console.error("FS operation failed");
   });
 
-  stream.on('data', (chunk) => {
+  stream.on("data", (chunk) => {
     hash.update(chunk);
   });
 
-  stream.on('end', () => {
-    console.log(hash.digest('hex'));
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
   });
 };
 
